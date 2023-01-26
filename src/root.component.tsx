4f9cea463b5469b7ed51cda60eb064b9b37d27a0
Martin Marzejon <martinmarzejon@Martins-MacBook-Pro.local>
2023-01-26 00:43:55 -0700

    feat: types

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,37 @@
+import { Nav } from "@hippobyte/stylekit";
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return (
+    <>
+      <Nav 
+        menu={[
+          {
+            key: 1,
+            title: "Overview",
+            path: "/",
+          },
+          {
+            key: 2,
+            title: "React",
+            path: "/react",
+          },
+          {
+            key: 3,
+            title: "Reactive",
+            path: "/reactive",
+          },
+          {
+            key: 4,
+            title: "Static HTML",
+            path: "/static-html",
+          },
+          {
+            key: 5,
+            title: "Layout Madness",
+            path: "/layout-madness",
+          }
+        ]}
+      />
+    </>
+  )
 }
